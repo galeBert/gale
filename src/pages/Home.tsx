1790f08f0c484326f7a20763e5eca88151649279
galeBert <76025267+galeBert@users.noreply.github.com>
2023-04-27 14:35:16 +0700

    feat(ui): mobile option 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import Hercules from '../assets/home-cover-clean.png'
+import HerculesBack from '../assets/full-back-hompage.png'
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { Banner } from '../component/Banner';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false)
@@ -35,14 +37,14 @@ export default function Home() {
     }
   }, [darkMode])
   return (
-    <div ref={ref} className='w-full h-full max-h-[698px] flex -mt-48 md:-mt-9 flex-col-reverse md:flex-row'>
-      <div className='w-full flex justify-center items-center font-clash'>
+    <div ref={ref} className='w-full h-screen flex max-h-[698px]  -mt-48 md:-mt-9 flex-col-reverse md:flex-row'>
+      <div className=' flex md:h-full h-1/3 w-full  justify-center items-center font-clash'>
         <div className='relative max-w-[417px] md:m-4 md:px-0 mx-8'>
           <div className='absolute rounded-full bg-orange-600 dark:bg-green-700 left-2 md:left-3 w-[26px] h-[26px] md:w-[46px] md:h-[46px] top-7  md:top-0 ' />
           <div className='pb-10 pt-6'>
         <span className='md:text-label-xl text-[24px] dark:text-primary-600 font-clashSemibold z-[1] relative'>Tailored </span><span className=' text-[18px] md:text-label-l font-clashSemibold dark:text-primary-600'>frontend Solutions to Drive Digital Success for Cutting-edge brands</span>
         </div>
-        <div className='relative'>       
+        <div className='relative hidden md:block'>       
         <p className='text-justify dark:text-primary-600'>
         Experienced Frontend Developer with 5+ Years of Expertise in React and TypeScript,
          Based in Bandung, Indonesia. Passionate about creating
@@ -53,9 +55,30 @@ export default function Home() {
         </div>
       
       </div>
-            <div  className='w-full overflow-hidden relative'>
-              <motion.button onClick={() => setDarkMode(!darkMode)} whileHover={'hover'} whileTap="pressed" className='w-full max-w-[466px]  transform ease-in-out  h-full max-h-[698px] -m-5 bg-orange-600 dark:bg-green-600'>
-              <motion.img style={{ y }} variants={ImageRef} alt='cover' className=' h-full' animate={{scale:1.2}} src={Hercules}/>
+            <div  className='w-full h-2/3 min-h-[400px] md:h-full overflow-hidden relative'>
+              {/* <div className='absolute z-20 top-20'>
+              <Banner title="DESIGN&nbsp;BY&nbsp;GALE&nbsp;*&nbsp;&nbsp;" className='font-clash  text-clip whitespace-nowrap text-heading-2xl' />
+              </div> */}
+             
+         
+      
+              <motion.button onClick={() => setDarkMode(!darkMode)} whileHover={'hover'} whileTap="pressed" className='w-full max-w-[466px] flex md:block items-end justify-center  transform ease-in-out md:rounded-none rounded-br-2xl rounded-bl-2xl  h-full max-h-[698px] bg-orange-600 dark:bg-green-600'>
+              <div className=' z-10 w-[2500px] md:hidden  h-full pointer-events-none absolute overflow-x-hidden left-0 top-20'>
+<div className={` animate-backwardMarquee flex w-full `}>
+        {Array.from({length: 4}).map((_, key) => {
+          return <Banner title="DESIGN&nbsp;BY&nbsp;GALE&nbsp;*&nbsp;&nbsp;" className='font-clash  text-clip whitespace-nowrap text-heading-2xl' />
+        } )}
+        </div>
+      </div>
+      <div className=' z-10 w-[2500px] md:hidden  h-full pointer-events-none absolute overflow-x-hidden left-0 top-44'>
+<div className={` animate-marquee flex w-full `}>
+        {Array.from({length: 4}).map((_, key) => {
+          return <Banner title="DESIGN&nbsp;BY&nbsp;GALE&nbsp;*&nbsp;&nbsp;" className='font-clash  text-clip whitespace-nowrap text-heading-2xl' />
+        } )}
+        </div>
+      </div>
+              <motion.img style={{ y }} variants={ImageRef} alt='cover' className=' h-full hidden md:block' animate={{scale:1.2}} src={Hercules}/>
+              <motion.img style={{ y }} variants={ImageRef} alt='cover' className=' absolute max-w-[210px] max-h-[225px] md:hidden z-50' src={HerculesBack}/>
               </motion.button>
             </div>
     </div>
